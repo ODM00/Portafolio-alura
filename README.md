14c502f0992b2793133338ecae993c42a57be065
oscar polo <zereck01@gmail.com>
2024-04-06 20:06:18 -0500

    Protype O.D.P.M

---
+++ README.md
@@ -0,0 +1,4 @@
+
+¡Primer desafío del Challenge Fron End!
+
+ Programado por Oscar D. Polo M.
---
+++ index.js
@@ -0,0 +1,7 @@
+import contactFormValidations from "./Modules/validaciones_formilario.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+  contactFormValidations();
+});
